5d1b5b89f604de72c6dd7ce4fef4c849e0ec4eac
SAOIO-T <vohoathuytien0403@gmail.com>
2025-08-30 13:34:08 +0700

    Add files via upload

---
+++ DemoNewsWeb/DemoNewsWeb/src/main/java/model/News.java
@@ -0,0 +1,33 @@
+package model;
+
+public class News {
+	private int id;
+	private String title;
+	private String content;
+
+	public News(int id, String title, String content) {
+		this.id = id;
+		this.title = title;
+		this.content = content;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public int getId() {
+		return id;
+	}
+}
